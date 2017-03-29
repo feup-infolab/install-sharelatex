e3faaca3c9117f6fe30b8bf3c39308f8c16ba3d0
João Rocha da Silva <joaorosilva@gmail.com>
2017-03-29 13:28:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,3 +42,6 @@ docker run -d --name shareredis -v /var/redis:/var/lib/redis redis
 docker run -d -P -p $PORT:80 -v /var/sharelatex_data:/var/lib/sharelatex --env SHARELATEX_MONGO_URL=mongodb://mongo/sharelatex --env SHARELATEX_REDIS_HOST=redis --env SHARELATEX_ADMIN_EMAIL=$SHARELATEX_ADMIN_EMAIL --link sharemongo:mongo --link shareredis:redis --name sharelatex sharelatex/sharelatex
 
 docker exec sharelatex /bin/bash -c "cd /var/www/sharelatex/; grunt user:create-admin --email $SHARELATEX_ADMIN_EMAIL" &&  echo "Installed sharelatex" || echo "Failed to install the sharelatex container"
+
+#install some extra packages (float, etc..)
+ docker exec sharelatex /bin/bash -c "sudo apt-get -y install texlive-generic-extra"
